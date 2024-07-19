dfcdd61ff361752d950229a72d1b7bed53a57cf9
Tomás <arrativeltomas@gmail.com>
2024-07-19 14:56:54 -0300

    Making routes and changing the connection of the db

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const ecomRoutes = require("./routes/index");
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world');
-})
+app.get("/", (req, res, next) => {
+	res.send("Hello world");
+});
+
+app.use("/api/ecommerce", ecomRoutes);
 
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+	console.log(`Server running on port: ${PORT}`);
 });
