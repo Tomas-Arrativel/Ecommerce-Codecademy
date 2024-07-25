93a7a3f08c4ea5331fd81d215e9bc4ff7da24453
Tomás <arrativeltomas@gmail.com>
2024-07-25 15:52:01 -0300

    Making a function to transform a money value to integer

--- src/shoppingCart/controller.js
+++ src/shoppingCart/controller.js
@@ -52,8 +52,36 @@ const deleteFromCart = (req, res) => {
 	}
 };
 
+const buyItemsInCart = (req, res) => {
+	const userId = req.session.userId;
+
+	if (userId) {
+		pool.query(queries.getProductsStored, [userId], (error, results) => {
+			if (error) throw error;
+			if (results.rows.length >= 1) {
+				let totalPrice = 0;
+
+				results.rows.forEach((product) => {
+					const newNumber =
+						parseInt(product.price.replace("$ ", "").replace(".", "")) *
+						product.quantity;
+					totalPrice += newNumber;
+					console.log(newNumber);
+				});
+
+				res.send(`${userId}, ${totalPrice}`);
+			}
+		});
+	} else {
+		res
+			.status(403)
+			.send("You need to log in to delete a product from the cart");
+	}
+};
+
 module.exports = {
 	getProductsStored,
 	addToCart,
 	deleteFromCart,
+	buyItemsInCart,
 };
--- src/shoppingCart/routes.js
+++ src/shoppingCart/routes.js
@@ -6,6 +6,8 @@ const router = Router();
 router.get("/", controller.getProductsStored);
 router.post("/", controller.addToCart);
 
+router.get("/checkout", controller.buyItemsInCart);
+
 router.delete("/:productId", controller.deleteFromCart);
 
 module.exports = router;
