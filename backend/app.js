426d8c036119785b4453bfbfcb2ff3c25f95f981
Tomás <arrativeltomas@gmail.com>
2024-08-21 15:58:28 -0300

    Making the functionality to log in and fixing something with the cors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 require("dotenv").config();
 const session = require("express-session");
+const cors = require("cors");
 const usersRoutes = require("./src/users/routes");
 const productsRoutes = require("./src/products/routes");
 const shoppingCartRoutes = require("./src/shoppingCart/routes");
 
 const app = express();
+
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+	})
+);
 
 app.use(express.json());
 app.use(
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState({ message: "" });
+
+	const navigate = useNavigate();
+
+	const onSubmit = async (e) => {
+		setLoading(true);
+		setError(false);
+		e.preventDefault();
+		try {
+			const res = await fetch("http://localhost:8000/api/users/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ username, password }),
+			});
+
+			// If the user can't log in, throw new error
+			if (!res.ok) {
+				setError({ message: "Something went wrong during the log in" });
+				throw new Error("Login failed");
+			}
+
+			const data = await res.json();
+
+			navigate("/");
+		} catch (error) {
+			setError({ error: true, message: error.message });
+			console.error("Error during login:", error.message);
+		}
+	};
+
 	return (
 		<div className="login">
-			<form className="login__form" action="">
+			<form className="login__form" onSubmit={onSubmit}>
 				<h2>Log in to your account</h2>
 				<div className="login__form-input">
 					<label htmlFor="username">Username:</label>
@@ -14,6 +49,8 @@ const Login = () => {
 						name="username"
 						id="usernameInput"
 						placeholder="Username"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
 					/>
 				</div>
 				<div className="login__form-input">
@@ -23,6 +60,8 @@ const Login = () => {
 						name="password"
 						id="passwordInput"
 						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
 					/>
 				</div>
 				<input type="submit" value="Log in" className="login__form-btn" />
