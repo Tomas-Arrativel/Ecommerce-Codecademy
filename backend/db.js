f35a3886ae3b04c8aee216822ec646a1a985f236
Tomás <arrativeltomas@gmail.com>
2024-09-23 16:50:27 -0300

    Db changes

--- backend/db.js
+++ backend/db.js
@@ -8,4 +8,12 @@ const pool = new Pool({
 	database: "ecommerce",
 });
 
+// const pool = new Pool({
+// 	host: process.env.DB_HOSTNAME,
+// 	user: process.env.DB_USER,
+// 	port: process.env.DB_PORT,
+// 	password: process.env.DB_NAME,
+// 	database: process.env.DB_PASSWORD,
+// });
+
 module.exports = pool;
